e795083fba9948e32609365eb83fcad739c5a245
ldvmitya <47355500+ldvmitya@users.noreply.github.com>
2019-02-08 20:14:22 +0300

    Update TaskDbContext.cs
    Change Database.Connection

--- Scheduler2/TaskDbContext.cs
+++ Scheduler2/TaskDbContext.cs
@@ -7,7 +7,7 @@ namespace MvcTasksApplication
     {
         public TaskDbContext()
         {
-            Database.Connection.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\User\source\repos\Scheduler2\Scheduler2\App_Data\MvcTasksApplication.ScheduleDbContext.mdf;Integrated Security=True;Connect Timeout=30";
+            Database.Connection.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\MvcTasksApplication.ScheduleDbContext.mdf;Integrated Security=True;Connect Timeout=30";
         }
 
         public DbSet<Schedule> Schedules { get; set; }
